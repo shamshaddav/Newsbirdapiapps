7dfa34189f25556334204da8590fb95241c3f969
shamshad <shamsaddav12@gmail.com>
2024-05-05 12:12:05 +0530

    second commit

---
+++ newsbird/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar'
+import Home from './Pages/Home';
+import FetchData from './components/FetchData';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <><Router>
+    <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/general' element={<FetchData cat='general' />} />
+        <Route path='/business' element={<FetchData cat='business' />} />
+        <Route path='/entertainment' element={<FetchData cat='entertainment' />} />
+        <Route path='/health' element={<FetchData cat='health' />} />
+        <Route path='/science' element={<FetchData cat='science' />} />
+        <Route path='/sports' element={<FetchData cat='sports' />} />
+        <Route path='/technology' element={<FetchData cat='technology' />} />
+      </Routes>
+      <Footer />
+    </Router>
+    </>
+  );
+};
+
+export default App
---
+++ newsbird/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='container fluid d-flex justify-content-center align-items-center bg-dark text-white p-3'>
+        <h5>
+            Developed by the<b> MD SHAMSHAD ANSARI</b> 
+            </h5>
+        </div>
+  )
+}
+
+export default Footer
